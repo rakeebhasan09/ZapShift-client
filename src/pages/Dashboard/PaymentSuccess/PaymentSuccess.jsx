6eec6d1b802021fc346f028e971db3a30c7f0681
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-11-23 00:23:07 +0600

    Generate transaction and tracking id on the server side and send it client side..

--- src/pages/Dashboard/PaymentSuccess/PaymentSuccess.jsx
+++ src/pages/Dashboard/PaymentSuccess/PaymentSuccess.jsx
@@ -1,12 +1,13 @@
 import { useSearchParams } from "react-router";
 import paymentSuccessImage from "../../../assets/payment-successfull.png";
 import Container from "../../../components/Container/Container";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
 	const [searchParams] = useSearchParams();
 	const axiosSecure = useAxiosSecure();
+	const [paymentInfo, setPaymentInfo] = useState({});
 	const sessionId = searchParams.get("session_id");
 	useEffect(() => {
 		if (sessionId) {
@@ -14,6 +15,10 @@ const PaymentSuccess = () => {
 				.patch(`/payment-success?session_id=${sessionId}`)
 				.then((res) => {
 					console.log(res.data);
+					setPaymentInfo({
+						trackingId: res.data.trackingId,
+						transactionId: res.data.transactionId,
+					});
 				});
 		}
 	}, [sessionId, axiosSecure]);
@@ -23,6 +28,8 @@ const PaymentSuccess = () => {
 			<section className="mt-4 md:mt-8 mb-7 md:mb-14 lg:mb-20">
 				<div className="container">
 					<Container>
+						<p>Transaction ID: {paymentInfo.transactionId}</p>
+						<p>Tracking ID: {paymentInfo.trackingId}</p>
 						<img
 							className="block w-3xl max-w-full mx-auto"
 							src={paymentSuccessImage}
